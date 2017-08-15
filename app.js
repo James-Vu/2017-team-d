53e901ffbe4c3f987228f0e5da4600bef7049d15
AndrewKuzminsky <kuzminskyandrew@gmail.com>
2017-08-15 17:54:58 +1000

    15/08/17 - Logo Added

--- app.js
+++ app.js
@@ -133,7 +133,6 @@ app.post('/', function(req, res) {
 app.listen(process.env.PORT || 3000, function () {
   console.log('Example app listening on port 3000!');
 });
-
 //localhost
 /*
 app.listen(3000, function() {
