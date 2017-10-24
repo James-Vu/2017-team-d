64fcd3e0e5eb5d4ffc2b0ab555d4b5ee4c83d654
Luke Lo Presti <luke-l@live.com>
2017-10-24 16:55:00 +1100

    add css to eliminated teams

--- app.js
+++ app.js
@@ -120,7 +120,7 @@ request(url, function(error, response, html) {
         var pointsAgainst = $(children[8]).text().trim();
         var percentage = $(children[9]).text().trim();
         var points = $(children[12]).text().trim();
-        var eliminated = position < 8
+        var eliminated = position > 8
 
 
         var team = {
